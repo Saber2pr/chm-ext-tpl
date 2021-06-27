58d6987ec57c14a34b36591c922671262c894058
saber2pr <saber2pr@gmail.com>
2021-06-27 15:46:46 +0800

    update

--- src/content.js
+++ src/content.js
@@ -1,3 +1,15 @@
+let x = 0
+let y = 0
+
+window.addEventListener('mousemove', event => {
+  x = event.clientX
+  y = event.clientY
+})
+
+const getClickElement = () => document.elementFromPoint(x, y)
+
 onMessage(data => {
-  console.log(data)
+  // 获取右键点击的元素
+  const element = getClickElement()
+  console.log('click', element)
 })
